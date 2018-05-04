bcc6270880d2b11e74ff372c863293ff016d0f93
JB <jb.durville@telecom-bretagne.eu>
2018-05-04 15:49:15 +0200

    get ethernet header!

--- main.c
+++ main.c
@@ -8,10 +8,23 @@
 #include <netinet/if_ether.h>
 #include <time.h>
 
+/* Convert 48 bit Ethernet ADDRess to ASCII.  */
+extern char *ether_ntoa (__const struct ether_addr *__addr) __THROW;
+extern char *ether_ntoa_r (__const struct ether_addr *__addr, char *__buf)
+     __THROW;
 
+/* Convert ASCII string S to 48 bit Ethernet address.  */
+extern struct ether_addr *ether_aton (__const char *__asc) __THROW;
+extern struct ether_addr *ether_aton_r (__const char *__asc,
+					struct ether_addr *__addr) __THROW;
+
+/* Map HOSTNAME to 48 bit Ethernet address.  */
+/* Map HOSTNAME to 48 bit Ethernet address.  */
+extern int ether_hostton (__const char *__hostname, struct ether_addr *__addr)
+     __THROW;
 
 void my_packet_handler(u_char *args,const struct pcap_pkthdr *header,const u_char *packet);
-void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header);
+u_int16_t handle_ethernet(const struct pcap_pkthdr* pkthdr, const u_char* packet);
 
 int main(int argc, char **argv)
 {
@@ -34,25 +47,52 @@ int main(int argc, char **argv)
 /*
 * Name : my_packet_handler
 * function :  function name that is the callback to be run on every packet captured
+*             then display packet info
 *
 */
 void my_packet_handler(u_char *args,const struct pcap_pkthdr *packet_header,const u_char *packet_body)
 {
-    print_packet_info(packet_body, *packet_header);
-    return;
+  //printf("Packet capture length: %d\n", packet_header->caplen);
+  printf("Packet total length %d\n", packet_header->len);
+
+  u_int16_t type = handle_ethernet(packet_header, packet_body);
+  
+  if(ntohs(type) == ETHERTYPE_IP) {
+    /* handle IP packet */
+    printf("IP Packet!\n");
+  }
 }
 
-/*
-* Name : print_packet_info
-* function :  display packet info
-*
-*/
-void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header) {
-    printf("Packet capture length: %d\n", packet_header.caplen);
-    printf("Packet total length %d\n", packet_header.len);
+u_int16_t handle_ethernet(const struct pcap_pkthdr* pkthdr, const u_char* packet) {
+    // http://yuba.stanford.edu/~casado/pcap/section4.html
+    struct ether_header *eptr;  /* net/ethernet.h */
+
+    /* lets start with the ether header... */
+    eptr = (struct ether_header *) packet;
+
+    fprintf(stdout,"ethernet header source: %s"
+            ,ether_ntoa((const struct ether_addr *)&eptr->ether_shost));
+    fprintf(stdout," destination: %s "
+            ,ether_ntoa((const struct ether_addr *)&eptr->ether_dhost));
+
+    /* check to see if we have an ip packet */
+    if (ntohs (eptr->ether_type) == ETHERTYPE_IP)
+    {
+        fprintf(stdout,"(IP)");
+    }else  if (ntohs (eptr->ether_type) == ETHERTYPE_ARP)
+    {
+        fprintf(stdout,"(ARP)");
+    }else  if (ntohs (eptr->ether_type) == ETHERTYPE_REVARP)
+    {
+        fprintf(stdout,"(RARP)");
+    }else {
+        fprintf(stdout,"(?)");
+        // exit(1);
+    }
+    fprintf(stdout,"\n");
+
+    return eptr->ether_type;
 }
-
-
 
 /* ask pcap to find a valid device for use to sniff on */
 /*  dev = pcap_lookupdev(errbuf);
