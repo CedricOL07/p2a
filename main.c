f08a25c187153d6f0a1c4eb24dfa074c121a3036
cedricOL07 <olivero@eurecom.fr>
2018-05-16 00:27:42 +0200

    num seq and num ack is wrong, to correct

--- main.c
+++ main.c
@@ -62,8 +62,8 @@ void my_packet_handler(u_char *args,const struct pcap_pkthdr *header,const u_cha
     	struct sniff_tcp {
     		u_short th_sport;	/* source port */
     		u_short th_dport;	/* destination port */
-    		tcp_seq th_seq;		/* sequence number */
-    		tcp_seq th_ack;		/* acknowledgement number */
+        tcp_seq th_seq;     /* sequence number */
+        tcp_seq th_ack;		/* acknowledgement number */
     		u_char th_offx2;	/* data offset, rsvd */
       	#define TH_OFF(th)	(((th)->th_offx2 & 0xf0) >> 4)
       	u_char th_flags;
@@ -138,10 +138,12 @@ void my_packet_handler(u_char *args,const struct pcap_pkthdr *header,const u_cha
       tcp_header = packet + ethernet_header_length +ip_header_length;
 
       tcp = (struct tcp *)(tcp_header);// move to the tcp layer  and we can get the information
-      //this is already there in the code:
+
           printf("Src port: %d\n", ntohs(tcp->th_sport));
           printf("Dst port: %d\n", ntohs(tcp->th_dport));
-      //you add:
+          printf("acknowledge number: %u\n", ntohs(tcp->th_ack));
+          printf("sequence number: %u\n", ntohs(tcp->th_seq));
+
           if (tcp->th_flags & TH_SYN){
               printf("Flag: TH_SYN\n");
           }
